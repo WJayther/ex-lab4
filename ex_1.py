101a51415ae5f8dd8ee044a86d45d3070183a6c4
WJayther <WalkingJinxed@gmail.com>
2016-10-27 19:57:56 +0300

    Proper one-line output
    Progress 3/6

--- ex_1.py
+++ ex_1.py
@@ -4,16 +4,19 @@ from librip.gens import gen_random
 
 goods = [
     {'title': 'Ковер', 'price': 2000, 'color': 'green'},
-    {'title': 'Диван для отдыха', 'price': 5300, 'color': 'black'},
+    {'title': 'Диван для отдыха', 'color': 'black'},
     {'title': 'Стелаж', 'price': 7000, 'color': 'white'},
-    {'title': 'Вешалка для одежды', 'price': 800, 'color': 'white'}
+    {'title': 'Вешалка для одежды', 'color': 'white'}
 ]
 
+def one_liner(arg):
+    print(', '.join(str(x) for x in arg))
+
 # Реализация задания 1
-test = field(goods,'title','absurd')
-
-for val in test:
-    print(val, end=',')
-
-for val in gen_random(1,5,5):
-    print(val, end=',')
+if __name__ == '__main__':
+    test = field(goods, 'title')
+    print(', '.join(str(x) for x in test))
+    #print(', '.join(str(x) for x in field(goods, 'title', 'price')))
+    #print(', '.join(str(x) for x in gen_random(1, 5, 5)))
+    one_liner(field(goods, 'title', 'price'))
+    one_liner(gen_random(1, 5, 5))
